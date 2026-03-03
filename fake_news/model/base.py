921c206158baf38dcdc61052062e8a1dd63d49b7
AngryCodes0404 <angrybird.codes@gmail.com>
2026-03-02 22:00:26 -0500

    remove description

--- fake_news/model/base.py
+++ fake_news/model/base.py
@@ -15,24 +15,12 @@ class Model(ABC):
               train_datapoints: List[Datapoint],
               val_datapoints: List[Datapoint],
               cache_featurizer: Optional[bool] = False) -> None:
-        """
-        Performs training of model. The exact train implementations are model specific.
-        :param train_datapoints: List of train datapoints
-        :param val_datapoints: List of validation datapoints that can be used
-        :param cache_featurizer: Whether or not to cache the model featurizer
-        :return:
-        """
+
         pass
     
     @abstractmethod
     def predict(self, datapoints: List[Datapoint]) -> np.array:
-        """
-        Performs inference of model on collection of datapoints. Returns an
-        array of model predictions. This should only be called after the model
-        has been trained.
-        :param datapoints: List of datapoints to perform inference on
-        :return: Array of predictions
-        """
+
         pass
     
     @abstractmethod
